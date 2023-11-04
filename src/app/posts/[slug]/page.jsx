60066ec228b82003675f843972e0b0d70ee8f6d0
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-11-04 18:13:43 +0100

    first deployment

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Comments from "@/components/comments/Comments";
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`https://kogi-province7.vercel.app/api/posts/${slug}`, {
     cache: "no-store",
   });
 
